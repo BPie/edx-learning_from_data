f398e5726de860d0db7a32c5be0c2ea195d3e061
Bartosz Piekarski <bartosz.piekarski@mail.com>
2016-09-29 01:02:17 +0200

    ADD scripts for hw1_{08,09,10}

---
+++ 1/hw1_08.py
@@ -0,0 +1,27 @@
+from pla import Pla2dRunner
+
+
+def main():
+    runs = 1000
+    prob_sum = 0
+    prob_testing_set_size = 1000
+    n = 10
+
+    for i in xrange(runs):
+        alg = Pla2dRunner(n)
+        while True:
+            done, step = alg.run_iteration()
+            if done:
+                current_prob = alg.get_prob(prob_testing_set_size)*100
+                prob_sum += current_prob
+                print "run ", i, ", prob: ", current_prob, " %"
+                break
+
+    print "================================="
+    print "results: "
+    print "================================="
+    print "average miss prob: ", prob_sum/float(runs), " % (", prob_sum/float(runs)/100., ")"
+
+
+if __name__ == "__main__":
+    main()
---
+++ 1/hw1_09.py
@@ -0,0 +1,24 @@
+from pla import Pla2dRunner
+
+
+def main():
+    runs = 1000
+    step_sum = 0
+    n = 100
+
+    for i in xrange(runs):
+        alg = Pla2dRunner(n)
+        while True:
+            done, step = alg.run_iteration()
+            if done:
+                step_sum += step
+                break
+
+    print "================================="
+    print "results: "
+    print "================================="
+    print "average steps needed: ", step_sum/float(runs)
+
+
+if __name__ == "__main__":
+    main()
